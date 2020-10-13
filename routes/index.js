aed2975cafc74b4df49d69fc290db28ef1de1a7a
naufalg <naufalghifari7@gmail.com>
2020-10-13 15:42:47 +0700

    add homepage text

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Welcome to home page');
+  res.send('Welcome to home page, \n Feature provided: \n get, get by id, post users in /users \n get profile data in /users/profile (after login in /login)');
 });
 
 module.exports = router;
